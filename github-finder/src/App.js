6f8a26df80fb69fd15aa5b85c59f18bf1b99a751
joker7t <batoan131196@gmail.com>
2020-05-18 00:21:23 +0700

    add navbar

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -5,11 +5,13 @@ import store from './store';
 import { Provider } from 'react-redux';
 import { HOME_PATH } from "./utils/constant";
 import Landing from './components/Landing';
+import MainNav from './components/layouts/MainNav';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
+        <MainNav icon="fa fa-github" title="Github Finder" />
         <div className="App">
           <Route exact path={HOME_PATH} component={Landing} />
         </div>
---
+++ github-finder/src/components/layouts/MainNav.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Navbar, Nav } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+
+class MainNav extends Component {
+    static defaultProps = {
+        icon: "fa fa-github",
+        title: "Github Finder"
+    }
+
+    render() {
+        const { title, icon } = this.props;
+
+        return (
+            <div>
+                <Navbar bg="primary" expand="lg">
+                    <Navbar.Brand href="#home">
+                        <i className={icon} /> {title}
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+MainNav.propTypes = {
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+}
+
+export default MainNav;
