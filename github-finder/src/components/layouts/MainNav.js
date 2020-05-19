2e4a2114c4b628044ac4c398d2ccf118ee859f71
joker7t <batoan131196@gmail.com>
2020-05-19 22:46:36 +0700

    refactor to stateless component

--- github-finder/src/components/layouts/MainNav.js
+++ github-finder/src/components/layouts/MainNav.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, Nav } from "react-bootstrap";
 import PropTypes from "prop-types";
 
 
-class MainNav extends Component {
-    static defaultProps = {
-        icon: "fa fa-github",
-        title: "Github Finder"
-    }
+const MainNav = props => {
 
-    render() {
-        const { title, icon } = this.props;
+    const { title, icon } = props;
 
-        return (
-            <div>
-                <Navbar bg="primary" expand="lg" variant="dark">
-                    <Navbar.Brand href="#home">
-                        <i className={icon} /> {title}
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+    return (
+        <div>
+            <Navbar bg="primary" expand="lg" variant="dark">
+                <Navbar.Brand href="#home">
+                    <i className={icon} /> {title}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
 
-                        </Nav>
+                    </Nav>
 
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-        );
-    }
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+MainNav.defaultProps = {
+    icon: "fa fa-github",
+    title: "Github Finder"
 }
 
 MainNav.propTypes = {
--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, Image, Button } from "react-bootstrap";
 
-class UserItem extends Component {
-    state = {
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-    }
+const UserItem = props => {
 
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
+    const { login, avatar_url, html_url } = props.user;
 
-        return (
-            <Card className="text-center p-1">
-                <Image
-                    variant="top"
-                    src={avatar_url}
-                    roundedCircle
-                    fluid
-                    className="user-img mx-auto"
-                />
-                <Card.Body>
-                    <h3>{login}</h3>
-                    <Button href={html_url} variant="dark" className="py-1">More</Button>
-                </Card.Body>
-            </Card>
-        );
-    }
+    return (
+        <Card className="text-center p-1">
+            <Image
+                variant="top"
+                src={avatar_url}
+                roundedCircle
+                fluid
+                className="user-img mx-auto"
+            />
+            <Card.Body>
+                <h3>{login}</h3>
+                <Button href={html_url} variant="dark" className="py-1">More</Button>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default UserItem;
