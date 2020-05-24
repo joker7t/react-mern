d2f86b5913d827497318a6947fe52b56ba66315a
joker7t <batoan131196@gmail.com>
2020-05-24 23:30:29 +0700

    refactor to deploy

--- github-finder/src/utils/auth.js
+++ github-finder/src/utils/auth.js
@@ -10,7 +10,5 @@ if (process.env.NODE_ENV !== 'production') {
     auth.clientId = process.env.GITHUB_CLIENT_ID;
     auth.clientSecret = process.env.GITHUB_CLIENT_SECRET;
 }
-auth.clientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-auth.clientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
 
 export default auth;
