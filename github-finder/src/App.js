07f4a0da018f52d0ae48a39e326d8107a75048fd
joker7t <batoan131196@gmail.com>
2020-05-24 23:55:56 +0700

    add Not FOund page: use switch to match first route

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import store from './store';
 import { Provider } from 'react-redux';
 import { HOME_PATH, USER_PATH } from "./utils/constant";
 import Landing from './components/Landing';
 import MainNav from './components/layouts/MainNav';
 import User from './components/users/User';
+import NotFound from "./components/layouts/NotFound";
 
 function App() {
   return (
@@ -14,8 +15,11 @@ function App() {
       <Router>
         <MainNav icon="fa fa-github" title="Github Finder" />
         <div className="App" style={{ marginTop: "70px" }}>
-          <Route exact path={USER_PATH} component={User} />
-          <Route exact path={HOME_PATH} component={Landing} />
+          <Switch>
+            <Route exact path={USER_PATH} component={User} />
+            <Route exact path={HOME_PATH} component={Landing} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
       </Router>
     </Provider>
---
+++ github-finder/src/components/layouts/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <React.Fragment>
+            <h1>Not Found</h1>
+            <p>The page you are looking is not found</p>
+        </React.Fragment>
+    );
+}
+
+export default NotFound;
