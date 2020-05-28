b12c98796ef579065b8063061f1911e3e2c942b0
joker7t <batoan131196@gmail.com>
2020-05-29 00:04:56 +0700

    add api for creating user

--- contact-keeper-react-mern-server/server.js
+++ contact-keeper-react-mern-server/server.js
@@ -6,6 +6,9 @@ const app = express();
 //connect databse
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
     res.json({
         msg: 'Welcome to Contact Keeper API...'
